57d39c26038adab93573738207c143dbd0814343
archie.cobbs <archie.cobbs@a5526da8-273f-c1e9-a3c8-d064aa815978>
2011-05-04 22:02:59 +0000

    Use dellroad-stuff versions for some ParseUtil methods.

--- trunk/src/java/org/dellroad/jibxbindings/ParseUtil.java
+++ trunk/src/java/org/dellroad/jibxbindings/ParseUtil.java
@@ -7,8 +7,6 @@
 
 package org.dellroad.jibxbindings;
 
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
@@ -29,78 +27,10 @@ import org.jibx.runtime.JiBXParseException;
  */
 public final class ParseUtil {
 
-    private static final String[] BOOLEAN_TRUES = { "1", "true", "yes" };
-    private static final String[] BOOLEAN_FALSES = { "0", "false", "no" };
-
     private static final Pattern RFC3339_FORMAT
       = Pattern.compile("(\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}(\\.\\d+)?)(Z|([-+]\\d{2}:\\d{2}))");
 
     private ParseUtil() {
-    }
-
-    /**
-     * Deserialize an {@link URI}.
-     *
-     * @see #serializeURI
-     */
-    public static URI deserializeURI(String string) throws JiBXParseException {
-        try {
-            return new URI(string);
-        } catch (URISyntaxException e) {
-            throw new JiBXParseException("invalid URI", string, e);
-        }
-    }
-
-    /**
-     * Serialize an {@link URI}.
-     *
-     * @see #deserializeURI
-     */
-    public static String serializeURI(URI uri) {
-        return uri.toString();
-    }
-
-    /**
-     * JiBX {@link String} deserializer that normalizes a string as is required by the {@code xsd:token} XSD type.
-     * This removes leading and trailing whitespace, and collapses all interior whitespace
-     * down to a single space character.
-     *
-     * @throws NullPointerException if {@code string} is null
-     */
-    public static String normalize(String string) {
-        return string.trim().replaceAll("\\s+", " ");
-    }
-
-    /**
-     * JiBX {@link String} deserializer support method that verifies that the input string matches the
-     * given regular expression. This method can be invoked by custom deserializers that supply the
-     * regular expression to it.
-     *
-     * @throws NullPointerException if {@code string} of {@code regex} is null
-     * @throws JiBXParseException if {@code string} does not match {@code regex}
-     * @throws java.util.regex.PatternSyntaxException if {@code regex} is not a valid regular expression
-     */
-    public static String deserializeMatching(String regex, String string) throws JiBXParseException {
-        if (!string.matches(regex))
-            throw new JiBXParseException("input does not match pattern \"" + regex + "\"", string);
-        return string;
-    }
-
-    /**
-     * Boolean parser that allows "yes" and "no" as well as the usual "true", "false", "0", "1".
-     *
-     * @throws JiBXParseException if the value is not recognizable as a boolean
-     */
-    public static boolean deserializeBoolean(String string) throws JiBXParseException {
-        for (String s : BOOLEAN_TRUES) {
-            if (string.equalsIgnoreCase(s))
-                return true;
-        }
-        for (String s : BOOLEAN_FALSES) {
-            if (string.equalsIgnoreCase(s))
-                return false;
-        }
-        throw new JiBXParseException("invalid Boolean value", string);
     }
 
     /**
